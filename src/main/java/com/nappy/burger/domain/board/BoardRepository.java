138ed8cbf313cd6e2aeecb74d24cbc5d9b8914ec
Nappy97 <wjddmlgud829@gmail.com>
2022-09-22 18:43:05 +0900

    0922

--- src/main/java/com/nappy/burger/domain/board/BoardRepository.java
+++ src/main/java/com/nappy/burger/domain/board/BoardRepository.java
@@ -5,12 +5,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
     @Modifying
     @Query("update Board p set p.count = p.count + 1 where p.id = :id")
-    int updateCount(Long id);
+    int updateCount(@Param("id") Long id);
 
     Page<Board> findByTitleContainingOrContentContaining(String title, String content, Pageable pageable);
 
