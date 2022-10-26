a22c5dd2c0dd845d7e99ebf191aae3d329d480bd
Nappy97 <wjddmlgud829@gmail.com>
2022-10-26 18:01:29 +0900

    1026 orderAvg half finished

--- src/main/resources/static/js/chart-bar-demo.js
+++ src/main/resources/static/js/chart-bar-demo.js
@@ -7,15 +7,14 @@ var ctx = document.getElementById("myBarChart");
 
 var myLineChart = new Chart(ctx, {
 
-
     type: 'bar',
     data: {
-        labels: burgerName,
+        labels: burgerArray,
         datasets: [{
             label: "Revenue",
             backgroundColor: "rgba(2,117,216,1)",
             borderColor: "rgba(2,117,216,1)",
-            data: [4215, 5312, 6251, 7841, 9821, 14984],
+            data: burgerArray1,
         }],
     },
     options: {
@@ -34,7 +33,7 @@ var myLineChart = new Chart(ctx, {
             yAxes: [{
                 ticks: {
                     min: 0,
-                    max: 15000,
+                    max: 500,
                     maxTicksLimit: 5
                 },
                 gridLines: {
